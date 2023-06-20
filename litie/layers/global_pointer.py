2a9f2dce5dfb1f5b443802024c5e428f610c1ac6
xusenlin <xusenlin@dnect.cn>
2023-06-21 00:22:57 +0800

    fix bug and update result

--- litie/layers/global_pointer.py
+++ litie/layers/global_pointer.py
@@ -271,48 +271,78 @@ class UnlabeledEntity(nn.Module):
 
 
 class HandshakingKernel(nn.Module):
-    def __init__(self, hidden_size, shaking_type="cln"):
+    def __init__(self, hidden_size, shaking_type, inner_enc_type="lstm"):
         super().__init__()
         self.shaking_type = shaking_type
-
         if shaking_type == "cat":
-            self.cat_fc = nn.Linear(hidden_size * 2, hidden_size)
-        if shaking_type == "cln":
+            self.combine_fc = nn.Linear(hidden_size * 2, hidden_size)
+        elif shaking_type == "cat_plus":
+            self.combine_fc = nn.Linear(hidden_size * 3, hidden_size)
+        elif shaking_type == "cln":
             self.tp_cln = LayerNorm(hidden_size, conditional_size=hidden_size)
+        elif shaking_type == "cln_plus":
+            self.tp_cln = LayerNorm(hidden_size, conditional_size=hidden_size)
+            self.inner_context_cln = LayerNorm(hidden_size, conditional_size=hidden_size)
 
-    def upper_reg2seq(self, tensor):
-        """
-        drop lower region and flat upper region to sequence
-        :param tensor: (batch_size, matrix_size, matrix_size, hidden_size)
-        :return: (batch_size, matrix_size + ... + 1, hidden_size)
-        """
-        bs, matrix_size, matrix_size, hidden_size = tensor.shape
-        mask = torch.ones(matrix_size, matrix_size, device=tensor.device).triu().bool()[None, :, :, None]
-        return tensor.masked_select(mask).reshape(bs, -1, hidden_size)
+        self.inner_enc_type = inner_enc_type
+        if inner_enc_type == "mix_pooling":
+            self.lamtha = nn.Parameter(torch.rand(hidden_size))
+        elif inner_enc_type == "lstm":
+            self.inner_context_lstm = nn.LSTM(
+                hidden_size,
+                hidden_size,
+                num_layers=1,
+                bidirectional=False,
+                batch_first=True,
+            )
+
+    def enc_inner_hiddens(self, seq_hiddens, inner_enc_type="lstm"):
+        # seq_hiddens: (batch_size, seq_len, hidden_size)
+        def pool(seqence, pooling_type):
+            if pooling_type == "mean_pooling":
+                pooling = torch.mean(seqence, dim=-2)
+            elif pooling_type == "max_pooling":
+                pooling, _ = torch.max(seqence, dim=-2)
+            elif pooling_type == "mix_pooling":
+                pooling = self.lamtha * torch.mean(seqence, dim=-2) + (1 - self.lamtha) * torch.max(seqence, dim=-2)[0]
+            return pooling
+
+        if "pooling" in inner_enc_type:
+            inner_context = torch.stack(
+                [pool(seq_hiddens[:, :i + 1, :], inner_enc_type) for i in range(seq_hiddens.size()[1])],
+                dim=1
+            )
+        elif inner_enc_type == "lstm":
+            inner_context, _ = self.inner_context_lstm(seq_hiddens)
+
+        return inner_context
 
     def forward(self, seq_hiddens):
         """
-        seq_hiddens: (batch_size, seq_len, hidden_size_x)
-        return:
-            if only look after:
-                shaking_hiddenss: (batch_size, (1 + seq_len) * seq_len / 2, hidden_size); e.g. (32, 5+4+3+2+1, 5)
-            else:
-                shaking_hiddenss: (batch_size, seq_len * seq_len, hidden_size)
+        :param seq_hiddens: (batch_size, seq_len, hidden_size)
+        :return: shaking_hiddenss: (batch_size, (1 + seq_len) * seq_len / 2, hidden_size) (32, 5+4+3+2+1, 5)
         """
-        seq_len = seq_hiddens.size(1)
+        seq_len = seq_hiddens.size()[-2]
+        shaking_hiddens_list = []
+        for ind in range(seq_len):
+            hidden_each_step = seq_hiddens[:, ind, :]
+            visible_hiddens = seq_hiddens[:, ind:, :]  # ind: only look back
+            repeat_hiddens = hidden_each_step[:, None, :].repeat(1, seq_len - ind, 1)
 
-        guide = seq_hiddens[:, :, None, :].repeat(1, 1, seq_len, 1)
-        visible = guide.permute(0, 2, 1, 3)
+            if self.shaking_type == "cat":
+                shaking_hiddens = torch.cat([repeat_hiddens, visible_hiddens], dim=-1)
+                shaking_hiddens = torch.tanh(self.combine_fc(shaking_hiddens))
+            elif self.shaking_type == "cat_plus":
+                inner_context = self.enc_inner_hiddens(visible_hiddens, self.inner_enc_type)
+                shaking_hiddens = torch.cat([repeat_hiddens, visible_hiddens, inner_context], dim=-1)
+                shaking_hiddens = torch.tanh(self.combine_fc(shaking_hiddens))
+            elif self.shaking_type == "cln":
+                shaking_hiddens = self.tp_cln([visible_hiddens, repeat_hiddens])
+            elif self.shaking_type == "cln_plus":
+                inner_context = self.enc_inner_hiddens(visible_hiddens, self.inner_enc_type)
+                shaking_hiddens = self.tp_cln([visible_hiddens, repeat_hiddens])
+                shaking_hiddens = self.inner_context_cln([shaking_hiddens, inner_context])
 
-        # guide, visible: (batch_size, shaking_seq_len, hidden_size)
-        guide = self.upper_reg2seq(guide)
-        visible = self.upper_reg2seq(visible)
-
-        if self.shaking_type == "cat":
-            tp_cat_pre = torch.cat([guide, visible], dim=-1)
-            shaking_pre = torch.relu(self.cat_fc(tp_cat_pre))
-
-        if self.shaking_type == "cln":
-            shaking_pre = self.tp_cln([visible, guide])
-
-        return shaking_pre
+            shaking_hiddens_list.append(shaking_hiddens)
+        long_shaking_hiddens = torch.cat(shaking_hiddens_list, dim=1)
+        return long_shaking_hiddens
